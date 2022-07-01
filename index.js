60dfda0b7abec77eeeba3fef8b16cb20ff69c5a4
ngocanh <lengocanhxd@gmail.com>
2022-07-01 23:23:27 +0700

    04 Thiết lập Status code

--- index.js
+++ index.js
@@ -13,16 +13,32 @@ const COURSE = [
 
 const server = http.createServer((req, res) => {
     // res.setHeader('Content-type', 'text/html'); // text/html
+    // res.setHeader('X-Powered-By', 'Nodejs');
     // res.write('Hello Word');
     // res.write('<h2>Hello World</h2>');
     // Neu tra ve dang json
     res.setHeader('Content-type', 'application/json')
 
-    
+    // res.statusCode = 200;
+    // res.end(JSON.stringify({
+    //     success: true,
+    //     data: COURSE
+    // }));
+
+    // res.statusCode = 404;
+
+    // Cach viet gon
+      res.writeHead(404, {
+        'Content-type': 'application/json',
+        'X-Powered-By': 'Nodejs'
+    })
     res.end(JSON.stringify({
-        success: true,
-        data: COURSE
+        success: false,
+        error: 'Page not found',
+        data: null
     }));
+
+  
 })
 
 const PORT = 5000;
